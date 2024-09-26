3416584649ba317f3a2cfd197977ab5af00a8105
Ismail Ilhomov <ismoil6ek@gmail.com>
2024-09-26 09:54:44 +0500

    feat: cli

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,9 @@
 const parseEnv = () => {
-    // Write your code here 
+  const rssVars = Object.entries(process.env).filter(([key]) =>
+    key.startsWith("RSS_")
+  );
+
+  console.log(rssVars.map(([key, value]) => `${key}=${value}`).join("; "));
 };
 
 parseEnv();
