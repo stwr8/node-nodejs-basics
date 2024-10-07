775e5fdf0ba4ab7ea59455e68f1c0beb5dff8876
Ismail Ilhomov <ismoil6ek@gmail.com>
2024-10-08 00:05:00 +0500

    feat: hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,20 @@
+import fs from "fs";
+import crypto from "crypto";
+
+const FILE_PATH = new URL("files/fileToCalculateHashFor.txt", import.meta.url);
+
 const calculateHash = async () => {
-    // Write your code here 
+  const hash = crypto.createHash("sha256");
+  const fileStream = fs.createReadStream(FILE_PATH);
+
+  fileStream.on("data", (chunk) => {
+    hash.update(chunk); // Update hash with the chunk of data
+  });
+
+  fileStream.on("end", () => {
+    const result = hash.digest("hex"); // Finalize the hash and get it in hex
+    console.log(result);
+  });
 };
 
 await calculateHash();
